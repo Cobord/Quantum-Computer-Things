dc48fc8717e0ea0adc41eda2476d73c19b14e2da
Ammar Husain <ammar.s.husain@gmail.com>
2018-10-30 16:46:02 -0700

    Comments
    Comments and the used case when B=A^c rather than two arbitrary subsets of vertices

--- expanders.hs
+++ expanders.hs
@@ -7,5 +7,10 @@ keepEdge thisEdge aSet bSet = (S.member (fst thisEdge) aSet) && (S.member (snd t
 connectingEdges :: G.Graph -> S.Set G.Vertex -> S.Set G.Vertex -> [G.Edge]
 connectingEdges graph aSet bSet = filter (\x -> keepEdge x aSet bSet) (G.edges graph)
 
+-- count number of edges connecting aSet and bSet and divide by the smaller's size
 cheegerAB :: G.Graph -> S.Set G.Vertex -> S.Set G.Vertex -> Rational
 cheegerAB graph aSet bSet = (fromIntegral $ length (connectingEdges graph aSet bSet))/(fromIntegral $ min (S.size aSet) (S.size bSet))
+
+-- B is the complement of A in actual computation of Cheeger constant
+cheegerA :: G.Graph -> S.Set G.Vertex -> Rational
+cheegerA graph aSet = cheegerAB graph aSet (S.difference allVertices aSet) where allVertices=foldr S.insert S.empty (G.vertices graph)
