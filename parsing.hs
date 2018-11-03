458d112b259acd6540fa70b09abd2df19894adba
Ammar Husain <ammar.s.husain@gmail.com>
2018-11-03 16:52:13 -0700

    Write Program
    From parsed write back in the specified , separated ; separated format

--- parsing.hs
+++ parsing.hs
@@ -57,6 +57,31 @@ readProgramHelper (Right y) = Right (fmap (fmap toInstr) y)
 readProgram :: String -> Either ParseError [[(InstructionSet,[Int])]]
 readProgram input = readProgramHelper $ parseCSV input
 
+writeProgramHelper2 :: [Int] -> String
+writeProgramHelper2 [] = ""
+writeProgramHelper2 (x:[]) = show x
+writeProgramHelper2 (x:xs) = show x ++ ";" ++ (writeProgramHelper2 xs)
+
+writeProgramHelper :: (InstructionSet,[Int]) -> String
+writeProgramHelper (PauliX1,onDigits) = "X"++(writeProgramHelper2 onDigits)
+writeProgramHelper (PauliY1,onDigits) = "Y"++(writeProgramHelper2 onDigits)
+writeProgramHelper (PauliZ1,onDigits) = "Z"++(writeProgramHelper2 onDigits)
+writeProgramHelper (Hadamard1,onDigits) = "H"++(writeProgramHelper2 onDigits)
+writeProgramHelper (QuarterPhase1,onDigits) = "QP"++(writeProgramHelper2 onDigits)
+writeProgramHelper (SqrtSwap2,onDigits) = "SQRTSWAP"++(writeProgramHelper2 onDigits)
+writeProgramHelper (CNOT2,onDigits) = "CNOT"++(writeProgramHelper2 onDigits)
+writeProgramHelper (Swap2,onDigits) = "SWAP"++(writeProgramHelper2 onDigits)
+writeProgramHelper (Tof3,onDigits) = "TOF"++(writeProgramHelper2 onDigits)
+
+writeProgram :: [(InstructionSet,[Int])] -> String
+writeProgram [] = "\n"
+writeProgram (x:[]) = (writeProgramHelper x) ++ "\n"
+writeProgram (x:xs) = (writeProgramHelper x) ++ "," ++ (writeProgram xs)
+
+writeManyPrograms :: Either ParseError [[(InstructionSet,[Int])]] -> String
+writeManyPrograms (Left x) = show x
+writeManyPrograms (Right x) = foldl (++) "" [writeProgram y | y <- x]
+
 --Example
 --readProgram "Z3,H1,Y2,H4,TOF1;2;4,CNOT1;52\n"
 -- will return Right [[(PauliZ1,[3]),(Hadamard1,[1]),(PauliY1,[2]),(Hadamard1,[4]),(Tof3,[1,2,4]),(CNOT2,[1,52])]]
@@ -65,5 +90,11 @@ readProgram input = readProgramHelper $ parseCSV input
 main = do  
         contents <- readFile "test.txt"
         let parsedPrograms = map readProgram . map (\x -> x ++ "\n") . words $ contents
-        let shownParsed = [(show x) ++ "\n" | x <- parsedPrograms]
-        writeFile "parsed_test.txt" (foldl (++) "" shownParsed)
+        let shownParsed = [writeManyPrograms x | x <- parsedPrograms]
+        let shownParsed2 = [(show x) ++ "\n" | x <- parsedPrograms]
+        writeFile "parsed_test.txt" (foldl (++) "" shownParsed2)
+        writeFile "refactored_test.txt" (foldl (++) "" shownParsed)
+
+-- The use case for this will be the naive program will be written in test.txt, it will get modified by something like compilerCurrent.hs
+-- and then written into refactored_test.txt
+-- the parsed_test.txt shows that it correctly read the data so that it could go into (GateData None) from compilerCurrent.hs
